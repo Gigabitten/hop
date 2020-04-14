b6af75b2dc0bd809c359db62ad65e478454cac5a
Caleb Spiess <spiesscaleb@gmail.com>
2020-04-14 00:09:18 -0600

    resolving conflicts...

--- src/screenControl.js
+++ src/screenControl.js
@@ -1,11 +1,7 @@
 import R from "./render.js";
 import F from "./frog.js";
 
-<<<<<<< HEAD
 const scrollSpeed = 0.06;
-=======
-const scrollSpeed = 0.04;
->>>>>>> abbb61de976fe2ae8a16f5e06af62806975a6e18
 
 let scroll = function() {
     R.xOffsetDelta.xO = (R.gameWidth / 2 - F.player.x) * scrollSpeed;
